c05805111e9a4dd5e741603e86f3f29222e66567
xiara125 <xiara125@gmail.com>
2021-11-25 13:55:00 +0900

    publish_image

--- fify/mqtt/subscriber.py
+++ fify/mqtt/subscriber.py
@@ -1,6 +1,5 @@
 import paho.mqtt.client as mqtt
 
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+ str(rc))
     if rc == 0:
@@ -18,7 +17,13 @@ def on_subscribe(client, userdata, mid, granted_qos):
 
 
 def on_message(client, userdata, msg):
-    print(str(msg.payload.decode("utf-8")))
+    # print(str(msg.payload.decode("utf-8")))
+    print("이미지 도착")
+    # img_string = msg.payload
+    # image_64_decode = base64.b64decode(img_string)
+    image_result = open('111.jpeg', 'wb')
+    image_result.write(msg.payload)
+    # image_result.close()
 
 
 client = mqtt.Client()
@@ -29,6 +34,7 @@ client.on_subscribe = on_subscribe
 client.on_message = on_message
 
 client.connect('18.142.131.188', 1883)
+# client.connect('192.168.35.200', 1883)
 
-client.subscribe('common2', 1)
-# client.loop_forever()
+client.subscribe('fify/image', 1)
+client.loop_forever()
